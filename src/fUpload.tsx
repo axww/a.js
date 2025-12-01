85148aa9dff4a5f23c061eedf81a0ebf95dc8ac3
axww <axww@github.com>
2025-12-01 19:13:29 +0900

    upload process

--- src/fUpload.tsx
+++ src/fUpload.tsx
@@ -1,12 +1,10 @@
 import { Context } from "hono";
-import { fileTypeFromBlob } from 'file-type';
 import { Config } from "./core";
 
 export async function fUpload(a: Context) {
     const file = await a.req.blob();
-    if (!(await fileTypeFromBlob(file))?.mime.startsWith('image/')) {
-        return a.text('Unsupported Media Type', 415);
-    }
+    if (file.size > 5242880) { return a.text('too_large', 413); } // 文件过大
+    if (!a.req.header('content-type')?.startsWith('image/')) { return a.text('image_only', 415); } // 不是图片
     const form = new FormData();
     form.append('reqtype', 'fileupload');
     form.append('userhash', await Config.get<string>(a, 'catbox_userhash'));
@@ -14,9 +12,9 @@ export async function fUpload(a: Context) {
     try {
         const response = await fetch('https://catbox.moe/user/api.php', { method: 'POST', body: form });
         if (response.ok) {
-            return a.text((await response.text()).split('/').at(-1) ?? '')
+            return a.text((await response.text()).split('/').at(-1) ?? '');
         } else {
-            return a.text(await response.text(), 500)
+            return a.text(await response.text(), 500);
         }
     } catch (error) {
         console.error('Upload failed:', error);
